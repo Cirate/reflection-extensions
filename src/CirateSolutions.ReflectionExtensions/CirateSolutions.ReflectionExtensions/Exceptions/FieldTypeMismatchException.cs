f89d4339af2234ae83a71afa65778e49d1a99a83
Cirate <mateuszlampart@outlook.com>
2019-08-10 17:01:34 +0200

    Exceptions have been created. Tests have been added.

---
+++ src/CirateSolutions.ReflectionExtensions/CirateSolutions.ReflectionExtensions/Exceptions/FieldTypeMismatchException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CirateSolutions.ReflectionExtensions.Exceptions
+{
+	public class FieldTypeMismatchException : FieldNotFoundException
+	{
+		public FieldTypeMismatchException(string fieldName, Type targetType, Type expectedFieldType)
+			: base(
+				fieldName,
+				targetType,
+				$"Field '{fieldName}' of '{expectedFieldType.Name}' type does not exist on '{targetType.Name}'")
+		{
+			ExpectedFieldType = expectedFieldType;
+		}
+
+		public Type ExpectedFieldType { get; }
+	}
+}
