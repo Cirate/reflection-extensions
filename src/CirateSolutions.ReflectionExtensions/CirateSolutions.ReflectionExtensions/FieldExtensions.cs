f87bde1999d908a2fa89c1dffc0f3c730d09b920
Cirate <30757446+Cirate@users.noreply.github.com>
2019-08-10 15:49:02 +0200

    Set up CI with Azure Pipelines (#1)
    * Solution has been created.
    * Azure pipeline has been created.
    * Status badge has been added.

---
+++ src/CirateSolutions.ReflectionExtensions/CirateSolutions.ReflectionExtensions/FieldExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace CirateSolutions.ReflectionExtensions
+{
+    public static class FieldExtensions
+    {
+	    public static TValue GetFieldValue<TValue>(this object target, string name)
+	    {
+		    target = target ?? throw new ArgumentNullException(nameof(target));
+
+		    var fieldInfo = target.GetType().GetField(name, BindingFlags.Instance | BindingFlags.NonPublic);
+
+		    if (fieldInfo is null)
+		    {
+			    throw new Exception();
+            }
+		    if (fieldInfo.FieldType != typeof(TValue))
+		    {
+			    throw new Exception();
+		    }
+
+		    return (TValue)fieldInfo.GetValue(target);
+        }
+    }
+}
