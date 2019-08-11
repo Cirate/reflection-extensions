b3327e223171c0482cfaa443b3953851ffe04a0e
Cirate <mateuszlampart@outlook.com>
2019-08-11 10:20:25 +0200

    Extension method for invoking void method has been added.

---
+++ src/CirateSolutions.ReflectionExtensions/CirateSolutions.ReflectionExtensions/MethodExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CirateSolutions.ReflectionExtensions.Exceptions;
+
+namespace CirateSolutions.ReflectionExtensions
+{
+	public static class MethodExtensions
+	{
+		private const BindingFlags InstanceMethods = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+
+		public static void Invoke(this object target, string methodName)
+		{
+			if (target is null)
+			{
+				throw new ArgumentNullException(nameof(target));
+			}
+
+			var methodInfo = target.GetType().GetMethod(methodName, InstanceMethods);
+
+			if (methodInfo is null)
+			{
+				throw new MethodNotFoundException(methodName, target.GetType());
+			}
+
+			if (methodInfo.ReturnType != typeof(void))
+			{
+				throw new MethodReturnTypeMismatchException(methodName, target.GetType(), typeof(void));
+			}
+
+			if (methodInfo.GetParameters().Any())
+			{
+				throw new MethodParametersMismatchException(methodName, target.GetType());
+			}
+
+			methodInfo.Invoke(target, null);
+		}
+	}
+}
---
+++ src/CirateSolutions.ReflectionExtensions/UnitTests/CirateSolutions.ReflectionExtensions.UnitTests/MethodExtensions/Invoke.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using CirateSolutions.ReflectionExtensions.Exceptions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CirateSolutions.ReflectionExtensions.UnitTests.MethodExtensions
+{
+	public class Invoke
+	{
+		[Test]
+		public void GivenMethodName_WhenItsPublic_InvokesMethod()
+		{
+			// arrange
+			const int expectedValue = -1;
+			var target = new Target();
+
+			// act
+			target.Invoke(Target.PublicVoidMethodName);
+
+			// assert
+			target.Counter.Should().Be(expectedValue);
+		}
+
+		[Test]
+		public void GivenMethodName_WhenItsPrivate_InvokesMethod()
+		{
+			// arrange
+			const int expectedValue = 1;
+			var target = new Target();
+
+			// act
+			target.Invoke(Target.PrivateVoidMethodName);
+
+			// assert
+			target.Counter.Should().Be(expectedValue);
+		}
+
+		[Test]
+		[SuppressMessage("ReSharper", "ExpressionIsAlwaysNull")]
+		public void GivenMethodName_WhenTargetIsNull_ThrowsArgumentNullException()
+		{
+			// arrange
+			Target target = null;
+
+			// act
+			Action act = () => target.Invoke(Target.PublicVoidMethodName);
+
+			// assert
+			act.Should().Throw<ArgumentNullException>();
+			act.Should().Throw<ArgumentNullException>().Which.ParamName.Should().Be("target");
+		}
+
+		[Test]
+		public void GivenMethodName_WhenItDoesNotExist_ThrowsMethodNotFoundException()
+		{
+			// arrange
+			var target = new Target();
+
+			// act
+			Action act = () => target.Invoke(Target.NonExistentMethodName);
+
+			// assert
+			act.Should().Throw<MethodNotFoundException>();
+			act.Should().Throw<MethodNotFoundException>().Which.MethodName.Should().Be(Target.NonExistentMethodName);
+			act.Should().Throw<MethodNotFoundException>().Which.TargetType.Should().Be(typeof(Target));
+		}
+
+		[Test]
+		public void GivenMethodName_WhenItsStaticMethod_ThrowsMethodNotFoundException()
+		{
+			// arrange
+			var target = new Target();
+
+			// act
+			Action act = () => target.Invoke(Target.StaticMethodName);
+
+			// assert
+			act.Should().Throw<MethodNotFoundException>();
+			act.Should().Throw<MethodNotFoundException>().Which.MethodName.Should().Be(Target.StaticMethodName);
+			act.Should().Throw<MethodNotFoundException>().Which.TargetType.Should().Be(typeof(Target));
+		}
+
+		[Test]
+		public void GivenMethodName_WhenItHasReturnType_ThrowsMethodReturnTypeMismatchException()
+		{
+			// arrange
+			var target = new Target();
+
+			// act
+			Action act = () => target.Invoke(Target.WithReturnTypeMethodName);
+
+			// assert
+			act.Should().Throw<MethodReturnTypeMismatchException>();
+			act.Should().Throw<MethodReturnTypeMismatchException>().Which.MethodName.Should().Be(Target.WithReturnTypeMethodName);
+			act.Should().Throw<MethodReturnTypeMismatchException>().Which.TargetType.Should().Be(typeof(Target));
+		}
+
+		[Test]
+		public void GivenMethodName_WhenItHasParameter_ThrowsMethodParametersMismatchException()
+		{
+			// arrange
+			var target = new Target();
+
+			// act
+			Action act = () => target.Invoke(Target.WithParameterMethodName);
+
+			// assert
+			act.Should().Throw<MethodParametersMismatchException>();
+			act.Should().Throw<MethodParametersMismatchException>().Which.MethodName.Should().Be(Target.WithParameterMethodName);
+			act.Should().Throw<MethodParametersMismatchException>().Which.ExpectedParameterTypes.Should().BeEmpty();
+			act.Should().Throw<MethodParametersMismatchException>().Which.TargetType.Should().Be(typeof(Target));
+		}
+
+		[SuppressMessage("ReSharper", "MemberCanBePrivate.Local")]
+		[SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
+		private class Target
+		{
+			public const string PublicVoidMethodName = nameof(Decrement);
+			public const string PrivateVoidMethodName = nameof(Increment);
+			public const string WithReturnTypeMethodName = nameof(WithReturnType);
+			public const string WithParameterMethodName = nameof(WithParameter);
+			public const string StaticMethodName = nameof(StaticMethod);
+			public const string NonExistentMethodName = "nonExistenMethod";
+
+			public int Counter { get; private set; }
+			public void Decrement() => Counter--;
+			public string WithReturnType() => string.Empty;
+
+			public void WithParameter(string parameter)
+			{
+			}
+
+			public static void StaticMethod()
+			{
+			}
+
+			private void Increment() => Counter++;
+		}
+	}
+}
